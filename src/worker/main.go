bab43ce3c7f392b9fbac86e5a625cf4d81f0c29a
Redskulling <widnes64@gmail.com>
2022-05-02 14:28:50 -0600

    Fix CNI paths

--- src/worker/main.go
+++ src/worker/main.go
@@ -12,8 +12,8 @@ func main() {
 	// These files must exist
 	opts.FcKernelImage = "ext/alpine.bin"
 	opts.FcRootDrivePath = "ext/rootfs.ext4"
-	opts.CNIConfigPath = "../cni/conf.d/fcnet.conflist"
-	opts.CNIPluginsPath = []string{"../cni/plugins"}
+	opts.CNIConfigPath = "cni/conf.d/"
+	opts.CNIPluginsPath = []string{"cni/plugins/"}
 	opts.CNINetnsPath = "ext/netns"
 
 	if err := runVM(context.Background(), opts); err != nil {
