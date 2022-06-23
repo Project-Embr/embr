dbddc33fc8b9e5abf0ddbccd7011e14ac51b831e
Redskulling <widnes64@gmail.com>
2022-06-23 16:31:28 -0600

    cleanup code

--- src/worker/vm.go
+++ src/worker/vm.go
@@ -43,7 +43,7 @@ func runVM(ctx context.Context, opts *options) error {
 		return fmt.Errorf("failed creating machine: %s", err)
 	}
 
-	cni(machine, opts)
+	startVeth(machine, opts)
 
 	if err := machine.Start(vmmCtx); err != nil {
 		return fmt.Errorf("failed to start machine: %v", err)
