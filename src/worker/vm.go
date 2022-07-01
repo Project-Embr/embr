521a3695987a98c84529c730025d9098d20bf230
Redskulling <36283015+Redskulling@users.noreply.github.com>
2022-06-30 19:02:26 -0600

    [Worker] Connect VM to CNI network interface (#6)
    * Started CNI Networking + go modules
    
    * Add CNI conf & plugin path to option struct
    
    * Add netns to options struct
    
    * Remove Submodules
    
    * Fix CNI paths
    
    * reverted changes
    
    * Started CNI Networking + go modules
    
    * add CNI conf & plugin path to option struct
    
    * Add netns to options struct
    
    * Remove Submodules
    
    * reverted changes
    
    * conflic resolution
    
    * fix actions with submodules
    
    * Finish Merge
    
    * cleanup code
    
    * accedental vscode file
    
    * move submodules
    
    * move submodules
    
    * unique netns for each machine

--- src/worker/vm.go
+++ src/worker/vm.go
@@ -43,6 +43,8 @@ func runVM(ctx context.Context, opts *options) error {
 		return fmt.Errorf("failed creating machine: %s", err)
 	}
 
+	startVeth(machine, opts)
+
 	if err := machine.Start(vmmCtx); err != nil {
 		return fmt.Errorf("failed to start machine: %v", err)
 	}
