665f78564ed9e16b337331a044af0cdbd275dd3f
Max Thomson <info@mthomson.ca>
2022-07-21 18:26:29 -0700

    Rename to master

--- src/master/frontend/tailwind.config.js
+++ src/master/frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*{js,ts,jsx,tsx}", "./components/**/*{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*{js,ts,jsx,tsx}",
+    "./src/components/**/*{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
